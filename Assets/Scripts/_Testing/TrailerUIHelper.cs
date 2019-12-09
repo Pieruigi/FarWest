8887155cd2b6b14de0a8837f9bb0528db3aeb161
PZ <pierluigi.zomparelli@gmail.com>
2019-12-09 11:35:17 +0100

    Removed credits sprites from resources to fix loading time issue; added button F to hive belt, clock and cursor when rigging trailer

---
+++ Assets/Scripts/_Testing/TrailerUIHelper.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailerUIHelper : MonoBehaviour
+{
+    bool isEnabled = true;
+
+    GameObject belt;
+    GameObject clock;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        belt = GameObject.FindObjectOfType<BeltUI>().gameObject;
+        clock = GameObject.FindObjectOfType<TimeUITest>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log("AAAAAAAAAAAAAAAAAAAAAA");
+
+            isEnabled = !isEnabled;
+
+            Cursor.visible = isEnabled;
+            belt.SetActive(isEnabled);
+            clock.SetActive(isEnabled);
+
+        }    
+    }
+}

65f2df484aa9037841731f5ae3fb93d9f2876f08
Pierpiero <pierluigi.zomparelli@gmail.com>
2020-07-13 09:42:49 +0200

    grindstone

---
+++ Assets/Scripts/Actions/ScreenSaver/Controllers/GrindstoneFreeTimeActionController.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrindstoneFreeTimeActionController : BaseFreeTimeActionController
+{
+    [SerializeField]
+    GameObject knifePrefab;
+
+    Transform handL, handR;
+
+    [SerializeField]
+    List<AudioClip> grindClips;
+
+    [SerializeField]
+    AudioSource grindSource;
+
+   // [SerializeField]
+    AudioSource source;
+
+
+
+
+    GameObject knife;
+
+    [SerializeField]
+    Animator grindAnimator;
+
+
+
+    protected override void Start()
+    {
+        base.Start();
+
+        handR = new List<Transform>(Player.GetComponentsInChildren<Transform>()).Find(p => "hand.r".Equals(p.name));
+        handL = new List<Transform>(Player.GetComponentsInChildren<Transform>()).Find(p => "hand.l".Equals(p.name));
+
+        source = GetComponent<AudioSource>();
+        
+    }
+
+    public override void ActionLoopStart(FreeTimeAction action, int loopId)
+    {
+        base.ActionLoopStart(action, loopId);
+
+       
+    }
+
+    public override void ActionMessage(string message)
+    {
+        base.ActionMessage(message);
+
+        if ("TakeKnife".Equals(message))
+        {
+            Debug.Log("Take Knife");
+            TakeKnife();
+        }
+
+        if ("ReleaseKnife".Equals(message))
+        {
+            ReleaseKnife();
+        }
+
+        if ("StartGrind".Equals(message))
+        {
+            StartGrind();
+        }
+
+        if ("StopGrind".Equals(message))
+        {
+            StopGrind();
+        }
+
+        if ("Grinding".Equals(message))
+        {
+            PlayGrinding();
+        }
+    }
+
+    
+    void StartGrind()
+    {
+        grindAnimator.SetBool("Start", true);
+        source.Play();
+
+    }
+
+    void StopGrind()
+    {
+        grindAnimator.SetBool("Start", false);
+        source.Stop();
+    }
+
+    void PlayGrinding()
+    {
+        grindSource.clip = grindClips[Random.Range(0, grindClips.Count)];
+        grindSource.Play();
+    }
+
+    void ReleaseKnife()
+    {
+        Utility.ObjectPopOut(knife);
+    }
+
+    void TakeKnife()
+    {
+        knife = Utility.ObjectPopIn(knifePrefab, Vector3.zero, Vector3.zero, Vector3.one, handR);
+
+    }
+   
+
+   
+
+    //void PlayClap()
+    //{
+    //    source.clip = clapClip;
+    //    source.Play();
+    //}
+}
